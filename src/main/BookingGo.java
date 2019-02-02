de66637c2546640a2d4e147d98d84d44f3b80e7a
Nikolett Bakos <nikolett.bakos@student.manchester.ac.uk>
2019-02-02 19:32:13 +0000

    Reorganise the project. Reduce the number of classes.

--- src/main/BookingGo.java
+++ src/main/BookingGo.java
@@ -26,6 +26,63 @@ public class BookingGo {
     }
 
     /**
+     * Function that handles with response of suppliers. If there is already an offer for a car type it compares it
+     * and keeps the cheaper otherwise adds it to the HashMap
+     * @param response Response that got from a supplier
+     * @param debug Debug file
+     * @param supplier Name of the current supplier
+     * @param offers HashMap including final offers
+     */
+    public static void getCarsAndPrices(String response, PrintWriter debug, String supplier,
+                                        Map<String, Offer> offers) {
+        // Get the useful information from the response
+        int firstIndex, lastIndex = 0;
+
+        while (response.indexOf("car_type", lastIndex) > 0) {
+            // Find the car type
+            firstIndex = response.indexOf("car_type", lastIndex);
+            lastIndex = response.indexOf("price", lastIndex + 1);
+            String car = response.substring(firstIndex + 11, lastIndex -3);
+
+            // Find the price corresponding to car type
+            firstIndex = response.indexOf("price", firstIndex);
+            lastIndex = response.indexOf('}', firstIndex + 1);
+            int price = Integer.parseInt(response.substring(firstIndex + 7, lastIndex));
+
+            // Create a new instance of Offer
+            Offer temp = new Offer (car, supplier, price);
+
+            // If there is already an offer for a car type it compares it and keeps the cheaper otherwise adds
+            // it to the HashMap
+            if(offers.containsKey(car)) {
+                debug.println("(ParameterAndResponseHandler) includes key " + car);
+                if (temp.getPrice() < offers.get(car).getPrice()) {
+                    debug.println("SWAP!!!!! Offer: " + offers.get(car).toString() + " to offer: " + temp.toString());
+                    offers.replace(car, temp);
+                }
+            }
+            else {
+                offers.put(car, temp);
+                debug.println("(ParameterAndResponseHandler) add key " + car + " and offer " + temp.toString());
+            }
+        }
+        debug.println();
+    }
+
+    /**
+     * Method to build URL with the given parameters
+     *
+     * @param supplier The required basic URL
+     * @param params Latitude and longitude of pick up and drop off locations
+     * @return The URL used to requests
+     */
+    public static String getURL(String supplier, Map<String, String> params) {
+        String URL = "https://techtest.rideways.com/";
+        URL += supplier + "?pickup=" + params.get("pickup") + "&dropoff=" + params.get("dropoff");
+        return URL;
+    }
+
+    /**
      * Implementation of bubble sort for array type Offer
      *
      * @param needToSort Offer type array that need to be sorted
@@ -58,7 +115,7 @@ public class BookingGo {
     private static void getTheOffersFromSuppliers (String supplier, Map<String, String> param,
                                                   PrintWriter debug, Map<String, Offer> offers) {
         // Get the required URL to the request
-        String supplierURL = ParameterAndResponseHandler.getURL(supplier, param);
+        String supplierURL = getURL(supplier, param);
 
         debug.println("(BookingGo) The URL for " + supplier + " is: " + supplierURL);
 
@@ -94,7 +151,7 @@ public class BookingGo {
                 debug.println("(BookingGo) Response: " + response.toString());
 
                 // Call function that handle with the response
-                ParameterAndResponseHandler.getCarsAndPrices(response.toString(), debug, supplier, offers);
+                getCarsAndPrices(response.toString(), debug, supplier, offers);
 
                 // Debug
                 debug.println("(BookingGo) Before sorting...");
